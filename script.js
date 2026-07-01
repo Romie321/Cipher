31dd67cb103f939ff6e82c377226001e9289d41b
Romie321 <raguilera321@gmail.com>
2026-07-01 00:07:44 -0700

    Refactor variable declarations and formatting for improved consistency

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-function rot13(str) {
-  let alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let resultStr = '';
+let encryptBtn = document.getElementById("encrypt-btn");
+let eInput = document.getElementById("encryptInput-1");
+let pInput = document.getElementById("plainInput");
+let inputs = [eInput, pInput];
+let copyBtn = document.getElementById("copyToClipBoard");
 
-  for(let i = 0; i < str.length; i++) {
+function rot13(str) {
+  let alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let resultStr = "";
+
+  for (let i = 0; i < str.length; i++) {
     let index = alpha.indexOf(str[i]);
-    if(index === -1) {
+    if (index === -1) {
       resultStr += str[i];
     } else {
       let secIndex = (index + 13) % 26;
