6315aa73f017f8a55ca904c823c53ed29bd91350
Romie321 <raguilera321@gmail.com>
2026-07-01 00:49:13 -0700

    Fix button ID for decryption functionality and update input field ID for consistency

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let encryptBtn = document.getElementById("encrypt-btn");
+let decryptBtn = document.getElementById("decrypt-btn");
 let eInput = document.getElementById("encryptInput-1");
-let pInput = document.getElementById("plainInput");
+let pInput = document.getElementById("plainInput-1");
 let inputs = [eInput, pInput];
 let copyBtn = document.getElementById("copyToClipBoard");
 
@@ -20,4 +20,4 @@ function rot13(str) {
   return resultStr;
 }
 
-console.log(rot13("SERR PBQR PNZC"));
+//console.log(rot13("SERR PBQR PNZC"));
